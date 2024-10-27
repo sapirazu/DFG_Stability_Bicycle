3c5cbc0ed9d3ec48c51ac1610402141e00653007
roee-zehavi <roee.zehavi@gmail.com>
2024-10-27 14:01:15 +0200

    הצלחתי להפעיל את המנועים!!

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
-import csv
-import cv2
-import sys
-import pyzed.sl as sl
-import cv_viewer.tracking_viewer as cv_viewer
-import numpy as np
-import math as m
+# import csv
+# import cv2
+# import sys
+# import pyzed.sl as sl
+# import cv_viewer.tracking_viewer as cv_viewer
+# import numpy as np
+# import math as m
 import time
-import socket
-from ctypes import *
-import random
-import mysql.connector
-from mysql.connector import RefreshOption
-import ogl_viewer.viewer as gl
+# import socket
+# from ctypes import *
+# import random
+# import mysql.connector
+# from mysql.connector import RefreshOption
+# import ogl_viewer.viewer as gl
 
 from edcon.edrive.com_modbus import ComModbus
 from edcon.edrive.motion_handler import MotionHandler
@@ -19,13 +19,27 @@ from edcon.utils.logging import Logging
 
 # Enable loglevel info
 Logging()
+# Create a list of all Modbus targets
+coms = [ComModbus(ip_address='192.168.0.11'),
+        ComModbus(ip_address='192.168.0.3')]
+mots = [MotionHandler(com) for com in coms]
 
-com = ComModbus('192.168.0.13')
-with MotionHandler(com) as mot:
+for mot in mots:
     mot.acknowledge_faults()
     mot.enable_powerstage()
-    mot.referencing_task()
+    if not mot.referenced():
+        mot.referencing_task()
 
-    mot.position_task(1, 1000)
-    mot.position_task(-5, 2000)
-    mot.position_task(10, 1500, absolute=True)
+for mot in mots:
+    mot.position_task(150000, 30,absolute=True, nonblocking=True) # 1cm=1000
+time.sleep(0.1)
+
+while True:
+    target_positions_reached = [mot.target_position_reached() for mot in mots]
+    Logging.logger.info(f"Target positions reached: {target_positions_reached}")
+    if all(target_positions_reached):
+        break
+    time.sleep(0.1)
+
+for mot in mots:
+    mot.shutdown()        
